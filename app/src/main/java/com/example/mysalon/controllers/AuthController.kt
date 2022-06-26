e03d548fa774c2b88c5a3aaf050858c34b74220f
Paula Caceres <paula.caceres.r@gmail.com>
2022-06-26 19:26:59 -0400

    hash

--- app/src/main/java/com/example/mysalon/controllers/AuthController.kt
+++ app/src/main/java/com/example/mysalon/controllers/AuthController.kt
@@ -10,25 +10,41 @@ import com.example.mysalon.AgendaActivity
 import com.example.mysalon.IngresoActivity
 import com.example.mysalon.ListadoClientesActivity
 import com.example.mysalon.lib.AppDatabase
+import com.example.mysalon.lib.BCrypt
 import com.example.mysalon.models.User
 import com.example.mysalon.models.UserEntity
 import java.util.*
 
+val INCORRECT_CREDENTIALS = "Credenciales incorrectas"
+
 class AuthController constructor(ctx: Context) {
     private val ctx = ctx
+    private val dao = Room.databaseBuilder(
+        ctx.applicationContext,
+        AppDatabase::class.java, "database-name"
+    )
+        .allowMainThreadQueries()
+        .build()
+        .userDao()
 
     fun login(email: String, password: String) {
-        if (email == "lis@gmail.com" && password == "123") {
-            Toast.makeText(this.ctx, "Bienvenido(a)", Toast.LENGTH_SHORT).show()
+        val user = dao.findByEmail(email)
+
+        if (user == null) {
+            Toast.makeText(this.ctx, INCORRECT_CREDENTIALS, Toast.LENGTH_SHORT).show()
+        }
+        if (BCrypt.checkpw(password, user.password)) {
+            Toast.makeText(this.ctx, "Bienvenido(a) ${user.nombre}", Toast.LENGTH_SHORT).show()
             val intent = Intent(this.ctx, AgendaActivity::class.java)
             this.ctx.startActivity(intent)
             (this.ctx as Activity).finish()
         } else {
-            Toast.makeText(this.ctx, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this.ctx, INCORRECT_CREDENTIALS, Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun registro( user: User){
+    fun registro(user: User) {
+        val hashedPassword = BCrypt.hashpw(user.password, BCrypt.gensalt())
         val userEntity = UserEntity(
             id = null,
             nombre = user.nombre,
@@ -37,24 +53,20 @@ class AuthController constructor(ctx: Context) {
             gender = user.gender,
             birth = user.birth,
             email = user.email,
-            password = user.password
+            password = hashedPassword
         )
 
-        val db = Room.databaseBuilder(
-            ctx.applicationContext,
-            AppDatabase::class.java, "database-name"
-        )
-            .allowMainThreadQueries()
-            .build()
 
-        val dao = db.userDao()
+        try {
+            dao.insert(userEntity)
 
-        dao.insert(userEntity)
-
-        Toast.makeText(this.ctx, "Cuenta registrada", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this.ctx, IngresoActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-        this.ctx.startActivity(intent)
+            Toast.makeText(this.ctx, "Cuenta registrada", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this.ctx, IngresoActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            this.ctx.startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(this.ctx, "Cuenta existente", Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun agregarCita(
@@ -63,7 +75,7 @@ class AuthController constructor(ctx: Context) {
         tipoServicio: String,
         fechaHora: String,
         hora: String
-    ){
+    ) {
         Toast.makeText(this.ctx, "Cita agregada exitosamente", Toast.LENGTH_SHORT).show()
         val intent = Intent(this.ctx, AgendaActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
@@ -76,7 +88,7 @@ class AuthController constructor(ctx: Context) {
         email: Editable?,
         cumpleanos: Editable?,
 
-    ){
+        ) {
         Toast.makeText(this.ctx, "Cliente agregado exitosamente", Toast.LENGTH_SHORT).show()
         val intent = Intent(this.ctx, ListadoClientesActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
