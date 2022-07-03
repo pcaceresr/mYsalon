15ae807d762b6ef4e58804c054b8938449a73ad0
Paula Caceres <paula.caceres.r@gmail.com>
2022-07-02 22:31:01 -0400

    conexion con base de datos login y registro

---
+++ app/src/main/java/com/example/mysalon/models/AuthDTO.kt
@@ -0,0 +1,23 @@
+package com.example.mysalon.models
+
+data class LoginPayloadDTO(
+    val identifier : String,
+    val password : String
+)
+
+data class UserDTO(
+    val id : Long
+
+)
+
+data class LoginResponseDTO(
+    val jwt : String,
+    val user : UserDTO
+
+)
+
+data class RegisterPayloadDTO(
+    val username : String,
+    val email : String,
+    val password: String
+)
